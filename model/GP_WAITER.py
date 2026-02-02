58b8f42aaf4e44916bc6129857f95ef1e0485fd3
snowo-w <fwe_heureux@163.com>
2026-02-02 14:58:08 +0800

    Update GP_WAITER.py

--- model/GP_WAITER.py
+++ model/GP_WAITER.py
@@ -9,23 +9,21 @@ class PatchEmbedding(nn.Module):
         self.conv2 = nn.Conv2d(1, 1, (60, 101), (1, 2))
         self.conv3 = nn.Conv2d(1, 1, (1, 101), (1, 1))
         self.conv4 = nn.Conv2d(1, 1, (1, 101), (1, 1))
-        self.sigmoid = nn.Sigmoid()
+        self.act1 = nn.Tanh()
+        self.act2 = nn.Tanh()
+        self.act3 = nn.Tanh()
         self.batchnorm2d = nn.BatchNorm2d(1)
         self.w=w
-        self.learned_w =nn.parameter(torch.rand(10))
+        
         
     def forward(self,x):
         x = x.unsqueeze(1)
-        l0=torch.mul(x,self.w).permute(0,2,3,1)
-        l0=torch.matmul(l0,self.w).permute(0,3,1,2)
+        l0=torch.mul(self.w,x)
         l1 = self.batchnorm2d(self.conv1(l0))
-        l2 = self.sigmoid(self.conv2(l1))
-        print("l2.shape:", l2.shape)
-        l3 = self.sigmoid(self.conv3(l2))
-        print("l3.shape:",l3.shape)
-        l4=self.sigmoid(self.conv4(l3))
+        l2 = self.act1(self.conv2(l1))
+        l3 = self.act2(self.conv3(l2))
+        l4=self.act3(self.conv4(l3))
         l4=self.batchnorm2d(l4)
-       
         return l4.squeeze()
 
 class AttentionBlock(nn.Module):
@@ -36,19 +34,19 @@ class AttentionBlock(nn.Module):
         self.layer_norm_2=nn.LayerNorm(embed_size1)
         self.linear_transformer=nn.Sequential(
             nn.Linear(embed_size1, embed_size2),
-            nn.Sigmoid()
+            nn.GELU()
         )
         self.linear=nn.Sequential(
             nn.Linear(embed_size1,embed_size2),
-            nn.Sigmoid(),
-            # nn.Dropout(drop_p),
-            nn.Linear(embed_size2,embed_size2)
+            nn.GELU(),
+            nn.Dropout(0.3),
+            nn.Linear(embed_size2,embed_size2),
+            nn.Dropout(0.3)
         )
     def forward(self,x):    
         x=self.layer_norm_1(x)
         x=x+self.attn(x,x,x)[0]
         weight=self.attn(x,x,x)[1]
-        print("weight.shape:",weight.shape)
         x=self.linear_transformer(x)+self.linear(self.layer_norm_2(x))
         return x
 
@@ -58,13 +56,27 @@ class TModel(nn.Module):
         super().__init__()
         self.patchembed=PatchEmbedding(w)
         self.layers=nn.Sequential(*(AttentionBlock(embed_size1=param[i]["embed_size1"],embed_size2=param[i]["embed_size2"],num_heads=param[i]["num_heads"]) for i in range(num_layers)))
-        self.out=nn.Sequential(nn.LayerNorm(embed_size),nn.Linear(embed_size,1),nn.Sigmoid())
-        self.out_r=nn.Sequential(nn.Conv1d(1,1,20,2),nn.Sigmoid(),nn.Conv1d(1,1,7,1),nn.Sigmoid())
-        self.ln=nn.Linear(3,1)
-        self.s=nn.Sigmoid()
+        self.out=nn.Sequential(nn.LayerNorm(embed_size),nn.Linear(embed_size,1),nn.GELU())
+        self.out_r=nn.Sequential(nn.Conv1d(1,1,20,2),nn.GELU(),nn.Conv1d(1,1,7,1),nn.GELU())
         self.batchnorm1d = nn.BatchNorm1d(1)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+                if m.bias is not None:
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='relu')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv1d):
+                init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
     def forward(self,x):
-        output=self.out(self.layers(self.patchembed(x))).squeeze()
-
-
-        return self.out_r(self.batchnorm1d(output.unsqueeze(1))).squeeze()
+        output=self.out(self.layers(self.patchembed_t(x))).squeeze()
+        output_final = self.out_r(self.batchnorm1d(output.unsqueeze(1))).squeeze()
+        return output_final
